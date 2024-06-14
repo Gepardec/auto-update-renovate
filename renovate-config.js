5b05dbbf2b489b90752f30988ea87fbcc3deb999
Christoph Ruhsam <christoph.ruhsam@gepardec.com>
2024-06-14 13:24:32 +0200

    add renovate default setup

---
+++ renovate-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+    platform: 'github',
+    repositories: [
+        "Ruhsi/gepardec-default-quarkus"
+    ],
+    "baseBranches": ["main"]
+}
