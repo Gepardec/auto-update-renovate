1e9938fc17e9128abed663c3dbfc69c67e9ffc00
Christoph Ruhsam <christoph.ruhsam@gepardec.com>
2024-06-17 08:17:54 +0200

    #5 add proxy config

--- renovate-config.js
+++ renovate-config.js
@@ -6,11 +6,21 @@ module.exports = {
     ],
     prHourlyLimit: 0,
     postUpgradeTasks: {
-        commands: ["./scripts/quarkus-update.sh"],
+        commands: ["./scripts/quarkus-update"],
         fileFilters: ["**/*", "**/.*"],
         executionMode: "branch"
     },
+    repositoryCache: true,
+    ignorePrAuthor: false, // default: false, set to true when migrating bot account
+    onboarding: false, // renovate.json files exist, so no onboarding MR is needed
+    requireConfig: true, // require a renovate.json or other valid renovate config be present
+    optimizeForDisabled: true, // first check in config if renovate is disabled
+    allowPostUpgradeCommandTemplating: true,
     recreateWhen: "always",
     allowScripts: true,
-    allowedPostUpgradeCommands: ["quarkus-update.sh", ".*"],
+    allowedPostUpgradeCommands: [
+        // array of regex strings of allowed `postUpgradeTasks` commands.
+        "^quarkus-update$"
+    ],
+
 }
