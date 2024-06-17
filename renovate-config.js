298db620b417ec359c606802b01da56c83d7041c
Christoph Ruhsam <christoph.ruhsam@gepardec.com>
2024-06-17 08:46:09 +0200

    #5 add proxy config

--- renovate-config.js
+++ renovate-config.js
@@ -6,7 +6,7 @@ module.exports = {
     ],
     prHourlyLimit: 0,
     postUpgradeTasks: {
-        commands: ["quarkus-update"],
+        commands: ["quarkus-update", "ls -l"],
         fileFilters: ["**/*", "**/.*"],
         executionMode: "update"
     },
