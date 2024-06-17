ea9a4832de16459a1a4e060cafa06b207356407e
Christoph Ruhsam <christoph.ruhsam@gepardec.com>
2024-06-17 10:50:35 +0200

    #5 add angular script

--- renovate-config.js
+++ renovate-config.js
@@ -1,14 +1,15 @@
 module.exports = {
-    "gitAuthor": "Renovate Bot <bot@renovateapp.com>",
-    "platform": "github",
-    "repositories": ["Ruhsi/gepardec-default-quarkus"],
-    "prHourlyLimit": 0,
-    "postUpgradeTasks": {
-        "commands": ["ls -l"],
-        "fileFilters": ["**/*.txt"],
-        "executionMode": "branch"
+    gitAuthor: "Renovate Bot <bot@renovateapp.com>",
+    platform: "github",
+    repositories: ["Ruhsi/gepardec-default-quarkus"],
+    prHourlyLimit: 0,
+    postUpgradeTasks: {
+        commands: ["ls -l"],
+        fileFilters: ["**/*.txt"],
+        executionMode: "branch"
     },
-    "recreateWhen": "always",
-    "allowScripts": true,
-    "allowedPostUpgradeCommands": ["touch", "ls", "ls -l"]
+    recreateWhen: "always",
+    allowScripts: true,
+    allowedPostUpgradeCommands: ["touch, ls, ls -l"],
+    cacheHardTtlMinutes: 1
 }
